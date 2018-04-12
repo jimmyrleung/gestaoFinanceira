da88b9bf296166074642db9d9211214ac08fd6c3
jimmyrl19 <jimmy.rl19@hotmail.com>
2018-04-12 15:57:59 -0300

    Loading all route files from a given directory

--- server/app/routes.loader.js
+++ server/app/routes.loader.js
@@ -1,7 +1,35 @@
-let planoFinanceiroRoutes = require("./planofinanceiro/planofinanceiro.route");
-let transacaoRoutes = require("./transacao/transacao.route");
+const fs = require("fs");
+const path = require("path");
+
 
 module.exports = function (express) {
-    planoFinanceiroRoutes(express);
-    transacaoRoutes(express);
+    let routeFiles = getRoutesFromDirectory(__dirname);
+
+    routeFiles.forEach((route) => {
+        require(route)(express);
+    });
+};
+
+function getRoutesFromDirectory(directory) {
+    let routes = [];
+    let routeFilesRegex = /.*route\.js$/;
+    let dirOrFileFullPath = "";
+
+    // Le o conteúdo do diretório
+    let directoryContent = fs.readdirSync(directory);
+
+    directoryContent.forEach((dirOrFile) => {
+        // Se não usar a linha abaixo irá buscar da raíz
+        dirOrFileFullPath = path.join(directory, dirOrFile);
+
+        // Se for um diretório busca arquivos '*route.js' nesse diretório
+        if (fs.lstatSync(dirOrFileFullPath).isDirectory()) {
+            routes = routes.concat(getRoutesFromDirectory(dirOrFileFullPath));
+        }
+        else {
+            if (routeFilesRegex.test(dirOrFileFullPath)) routes = routes.concat(dirOrFileFullPath);
+        }
+    });
+
+    return routes;
 }
